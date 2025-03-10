f5dc53fe0331593abaeaddb5c6125d3e0d1db62d
Phillip-Kemper <33121795+Phillip-Kemper@users.noreply.github.com>
2025-03-11 11:10:49 +1300

    change to typescript module

--- dist/server/index.js
+++ dist/server/index.js
@@ -1,39 +1,27 @@
 import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from 'zod';
-
 // Create an MCP server
 const server = new McpServer({
-  name: "Demo",
-  version: "1.0.0"
+    name: "Demo",
+    version: "1.0.0"
 });
-
 // Add an addition tool
-server.tool("add",
-  { a: z.number(), b: z.number() },
-  async ({ a, b }) => ({
+server.tool("add", { a: z.number(), b: z.number() }, async ({ a, b }) => ({
     content: [{ type: "text", text: String(a + b) }]
-  })
-);
-
+}));
 // Add a dynamic greeting resource
-server.resource(
-  "greeting",
-  new ResourceTemplate("greeting://{name}", { list: undefined }),
-  async (uri, { name }) => ({
+server.resource("greeting", new ResourceTemplate("greeting://{name}", { list: undefined }), async (uri, { name }) => ({
     contents: [{
-      uri: uri.href,
-      text: `Hello, ${name}!`
-    }]
-  })
-);
-
+            uri: uri.href,
+            text: `Hello, ${name}!`
+        }]
+}));
 // Wrap the code that uses await in an async function
 async function main() {
-  // Start receiving messages on stdin and sending messages on stdout
-  const transport = new StdioServerTransport();
-  await server.connect(transport);
+    // Start receiving messages on stdin and sending messages on stdout
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
 }
-
 // Call the async function
 main().catch(console.error);
